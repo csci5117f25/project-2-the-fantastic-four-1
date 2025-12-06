a63c8811c396057b2be888fd2cc5852d6dcc0166
Cameron Nagle <camnag01@gmail.com>
2025-12-06 16:23:10 -0600

    Testing Installable

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,46 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-    server: {
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "My Vue App",
+        short_name: "VueApp",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#42b883",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable"
+          }
+        ]
+      }
+    }),
+  ],
+  server: {
     host: '0.0.0.0',
-    port: 5173
-  }
+    port: 5173,
+  },
 })
